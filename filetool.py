5b33e3cb9774679fef6dd9ec37b3a4d30cadf209
CallSign <stjohngiddy@gmail.com>
2012-10-02 16:46:03 +0200

    Added UserConfig() class. Not used yet.

--- filetool.py
+++ filetool.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-#Name: Locks - Core Web Server
-#FileName: CoreHTTPServer.py
-__version__ = "0.1.0"
+#Name: File Tool - File I/O Tool for Locks Web Server
+#FileName: filetool.py
+__version__ = "0.1.1"
 __all__ = ["FileTool"] 
 """ 
 File Tool
@@ -12,4 +12,29 @@ Tool for reading and managing files with Locks (0.3.0)
 
 import os
 import sys
+
+class ReadErrorPage():
+    """Read error pages """
+    def Error404(self):
+        pass
+        
+class UserConfig():
+    """Config file """
+    
+    def MakeConfig(self):
+        template = 'DocRoot=www\nErr404=Error404.html\nNoIndexError=NoIndex.html\nPort=9000'
+        config = open('config.txt', 'r+')
+        config.write(template)
+        config.close()
+        
+    def ReadConfig(self): #XXX: Need to make sure what actual option it is returning value for.
+        config = open('config.txt', 'r')
+        returnlist = []
+        for line in config:
+            [x, y] = line.split('=')
+            returnlist.append(y)
+        config.close()
+        return returnlist # Return values in the format: Doc_root, Err404, NoIndex, Port
+
+
 

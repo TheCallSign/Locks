cd168b79f4200149f460db84978d539334bd9493
CallSign <stjohngiddy@gmail.com>
2012-10-02 11:26:54 +0200

    Created Module (Version 0.1.0). Now using a VCS.

---
+++ filetool.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#Name: Locks - Core Web Server
+#FileName: CoreHTTPServer.py
+__version__ = "0.1.0"
+__all__ = ["FileTool"]
+""" 
+File Tool
+
+Tool for reading and managing files with Locks (0.3.0)
+
+"""
+
+import os
+import sys
+
